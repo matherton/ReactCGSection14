0af83239034fa9f413b1c3f3de73240c756979c1
Mark Atherton <mark@insightinternet.co.uk>
2021-11-03 12:04:16 +0000

    end of lesson 177: Changed fetch request function to use async instead of chaining .then's

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import "./App.css";
 function App() {
   const [movies, setMovies] = useState([]);
 
-  function fetchMoviesHandler() {
+  /*function fetchMoviesHandler() {
     fetch("https://swapi.dev/api/films/")
       .then((response) => {
         return response.json();
@@ -22,6 +22,20 @@ function App() {
         });
         setMovies(transformedMovies);
       });
+  }*/ //is same as shorthand:
+  async function fetchMoviesHandler() {
+    const response = await fetch("https://swapi.dev/api/films/");
+    const data = await response.json();
+
+    const transformedMovies = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date,
+      };
+    });
+    setMovies(transformedMovies);
   }
 
   return (
