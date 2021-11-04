6c293ba776350dd875cabf18e8af95cd1252525f
Mark Atherton <mark@insightinternet.co.uk>
2021-11-04 13:26:12 +0000

    moved moved fetch request into a try block with blank catch to deal with errors

--- src/App.js
+++ src/App.js
@@ -28,19 +28,21 @@ function App() {
   async function fetchMoviesHandler() {
     setIsLoading(true);
     setError(null);
-    const response = await fetch("https://swapi.dev/api/film/");
-    const data = await response.json();
+    try {
+      const response = await fetch("https://swapi.dev/api/film/");
+      const data = await response.json();
 
-    const transformedMovies = data.results.map((movieData) => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date,
-      };
-    });
-    setMovies(transformedMovies);
-    setIsLoading(false);
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+      setIsLoading(false);
+    } catch (error) {}
   }
 
   return (
