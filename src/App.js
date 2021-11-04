112c1a159db94bda870c9f9561ad8c67c4d3c2be
Mark Atherton <mark@insightinternet.co.uk>
2021-11-04 12:55:09 +0000

    added isLoading conditions in JSX to display <MoviesList /> or Loading... text

--- src/App.js
+++ src/App.js
@@ -47,7 +47,8 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && <MoviesList movies={movies} />}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
